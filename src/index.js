64f396bba3f5801bc74d41f864b05dc5318c861e
sagar.pawar <sa7ar.vijaya@gmail.com>
2022-04-09 13:57:16 +0530

    Initial check-in

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import reportWebVitals from './reportWebVitals';
+
+export * from './lib/components/UI/Button/Button'
+export * from './lib/components/UI/Input/Input'
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
